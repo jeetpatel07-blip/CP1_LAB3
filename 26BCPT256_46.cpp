36dc47d0e65d0066bc0ac81e1b33b02eeac2d262
jeetpatel07-blip <jeetpatel07@gmail.com>
2026-09-01 16:40:36 +0530

    Update 26BCPT256_46.cpp

--- 26BCPT256_46.cpp
+++ 26BCPT256_46.cpp
@@ -2,20 +2,25 @@
 int main ()
 
 {
-    int a,b=0,d;
-    printf("------LARGEST AMONG 100 NUMBERS------\n\n\n\n")
+    int a,b=0,d,c=d;
+    printf("------LARGEST AMONG 100 NUMBERS------\n\n\n\n");
 
-    for (a=1;a<=100;a++)
+    for (a=1;a<=5;a++)
     {
         printf("%d no enter : ",a);
         scanf("%d",&d);
 
+
         if (b<d)
            {b=d;}
+        if (c>d)
+            {c=d;}
 }
 
 
-printf("%d is the largest number",b);
+printf("%d is the largest number\n",b);
+printf("%d is the smallest number",c);
+
 return 0 ;
 
 }
